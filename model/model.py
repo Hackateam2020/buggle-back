07c2097a3146d3240a2a6ceb64f8cf08b7bad47d
MiguelMJ <miguelmejiajimenez@hotmail.com>
2020-10-18 17:04:21 +0200

    model integrated

--- model/model.py
+++ model/model.py
@@ -5,6 +5,20 @@ filename = 'model/prediction_model'
 loaded_model = pickle.load(open(filename,'rb'))
 
 dicto = {'gender_type':0, 'birth_date':1990, 'income_monthly_amount': 1000, 'deudas_banca': 0, 'banca_movil': 1, 'Sunat':0}
-ended = pd.DataFrame(data=dicto,index=[0])
-predict = loaded_model.predict(ended)
-print(f'Según nuestros datos tu solicitud ha sido: {predict}')
+# ended = pd.DataFrame(data=dicto,index=[0])
+# predict = loaded_model.predict(ended)
+# print(f'Según nuestros datos tu solicitud ha sido: {predict}')
+
+def prediction(form):
+    dicto['gender_type'] = form['género'] == 'masculino' and 1 or 0
+    dicto['birth_date'] = int(form['año'])
+    dicto['income_monthly_amount'] = int(form['ingresos'])
+    dicto['deudas_banca'] = form['ingresos'] == 'si' and 1 or 0
+    dicto['banca_movil'] = form['celular'] == 'si' and 1 or 0
+    dicto['Sunat'] = form['deudasunat'] == 'si' and 1 or 0
+    ended = pd.DataFrame(data=dicto, index=[0])
+    result = loaded_model.predict(ended)
+    return result[0] == 'Aprobado'
+    
+    
+    
